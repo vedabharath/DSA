a08bd27e76f61bd4a2f31c209c7fb2b0db56776a
Bharath-PC <bharathchinthakayala6@gmail.com>
2022-08-09 13:10:13 +0530

    weekly contest 305

--- src/main/java/com/bharath/skills/main/ValidPartition.java
+++ src/main/java/com/bharath/skills/main/ValidPartition.java
@@ -1,6 +1,17 @@
 package com.bharath.skills.main;
 
 public class ValidPartition {
+
+    public boolean validPartition2(int[] n) {
+        boolean[] dp = {true, false, n[0] == n[1], false};
+        for (int i = 2; i < n.length; ++i) {
+            boolean two = n[i] == n[i - 1];
+            boolean three = (two && n[i] == n[i - 2]) || (n[i] - 1 == n[i - 1] && n[i] - 2 == n[i - 2]);
+            dp[(i + 1) % 4] = (two && dp[(i - 1) % 4]) || (three && dp[(i - 2) % 4]);
+        }
+        return dp[n.length % 4];
+    }
+
     public boolean validPartition(int[] a) {
         
         int n = a.length;
